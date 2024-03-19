fcd9f581de7a3b1f39551873fcc8f974eb9ff087
Sergio <sergio.hanganu@gmail.com>
2024-03-19 18:14:02 +0100

    The version whit the null exeption

--- MainTest.java
+++ MainTest.java
@@ -3,13 +3,21 @@ import org.junit.jupiter.api.Test;
 import java.time.OffsetDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class MainTest {
     @Test
-    public void dataTest() {
+    public void dataTestBasic() {
         OffsetDateTime testOne = OffsetDateTime.parse("2023-03-01T13:00:00Z");
         String formatDate = Main.dataTimeFormact(testOne);
         String expected = "01 marzo 2023";
         assertEquals(expected, formatDate);
     }
 
+    @Test
+    public void dataTestNull() {
+        OffsetDateTime dateTime = null;
+        assertThrows(NullPointerException.class, () ->
+                Main.dataTimeFormact(dateTime)
+        );
+    }
 }
